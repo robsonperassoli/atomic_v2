218945da8710e81490110f7803de50724360063f
Robson Gian Perassoli <robsonperassoli@gmail.com>
2022-08-31 11:41:57 -0300

    feat(ui:auth): remove middleware redirect

--- atomic_ui/config.js
+++ atomic_ui/config.js
@@ -1,4 +1,6 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api/graphql'
+  process.env.NEXT_PUBLIC_API_URL ||
+  'http://api.atomic.localhost:4000/api/graphql'
 export const GITHUB_AUTH_URL =
-  process.env.NEXT_PUBLIC_GITHUB_AUTH_URL || 'http://localhost:4000/auth/github'
+  process.env.NEXT_PUBLIC_GITHUB_AUTH_URL ||
+  'http://api.atomic.localhost:4000/auth/github'
