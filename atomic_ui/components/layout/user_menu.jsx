5b5722d14d94d3581ec00f2866cf90acbc1e2f82
Robson Gian Perassoli <robsonperassoli@gmail.com>
2022-09-14 17:22:39 -0300

    feat(ui): add logout to user menu

--- atomic_ui/components/layout/user_menu.jsx
+++ atomic_ui/components/layout/user_menu.jsx
@@ -1,20 +1,41 @@
 import Image from 'next/image'
+import { Menu } from '@headlessui/react'
+import { LOGOUT_URL } from '../../config'
+import classNames from 'classnames'
 
 const UserMenu = () => {
   return (
-    <button className="flex items-center self-stretch px-2 mr-2 gap-2 text-white hover:bg-gray-800 hover:bg-opacity-25 transition-colors duration-200">
-      <figure className="rounded-full overflow-hidden flex">
-        <Image
-          src="/images/cat.svg"
-          width={50}
-          height={50}
-          alt="user profile picture"
-        />
-      </figure>
+    <Menu as="div" className="relative self-stretch mr-2">
+      <Menu.Button className="flex items-center h-full px-2 gap-2 text-white hover:bg-gray-800 hover:bg-opacity-25 transition-colors duration-200">
+        <figure className="rounded-full overflow-hidden flex">
+          <Image
+            src="/images/cat.svg"
+            width={50}
+            height={50}
+            alt="user profile picture"
+          />
+        </figure>
 
-      <span className="text-lg medium tracking-wide">Robson Perassoli</span>
-      <i className="fas fa-chevron-down text-xl" />
-    </button>
+        <span className="text-lg medium tracking-wide">Robson Perassoli</span>
+        <i className="fas fa-chevron-down text-xl" />
+      </Menu.Button>
+      <Menu.Items className="absolute right-2 origin-top-right mt-1 w-56 rounded-lg bg-white shadow-lg z-10 overflow-hidden">
+        <Menu.Item>
+          {({ active }) => (
+            <a
+              className={classNames(
+                'p-3 font-medium  block text-gray-800',
+                active && 'bg-slate-100'
+              )}
+              href={LOGOUT_URL}
+            >
+              <i className="fas fa-person-running text-purple-600 text-lg mr-2" />
+              Logout
+            </a>
+          )}
+        </Menu.Item>
+      </Menu.Items>
+    </Menu>
   )
 }
 
--- atomic_ui/config.js
+++ atomic_ui/config.js
@@ -1,6 +1,11 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL ||
   'http://api.atomic.localhost:4000/api/graphql'
+
 export const GITHUB_AUTH_URL =
   process.env.NEXT_PUBLIC_GITHUB_AUTH_URL ||
   'http://api.atomic.localhost:4000/auth/github'
+
+export const LOGOUT_URL =
+  process.env.NEXT_PUBLIC_LOGOUT_URL ||
+  'http://api.atomic.localhost:4000/auth/logout'
